81bf91907057cc338f836b217b2c087ed56f4a49
crifan <admin@crifan.com>
2017-09-28 16:02:10 +0800

    1. add CrifanCollection

---
+++ swift/CrifanCollection.swift
@@ -0,0 +1,13 @@
+//
+//  CrifanCollection.swift
+//  Crifan Li
+//  Updated: 2017/09/28
+//
+
+import Foundation
+
+extension Collection {
+    var notEmpty: Bool {
+        return !self.isEmpty
+    }
+}
