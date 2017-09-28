17e5f1bdba0ff4d430451258143dd2789c26e433
crifan <admin@crifan.com>
2017-09-28 15:57:54 +0800

    1. update CrifanUITableViewCell

--- swift/CrifanLibUI/CrifanUITableViewCell.swift
+++ swift/CrifanLibUI/CrifanUITableViewCell.swift
@@ -1,9 +1,7 @@
 //
 //  CrifanUITableViewCell.swift
-//  SalesApp
-//
-//  Created by licrifan on 16/6/21.
-//  Copyright © 2016年 licrifan. All rights reserved.
+//  Crifan Li
+//  Updated: 2017/09/28
 //
 
 import UIKit
@@ -22,36 +20,21 @@ extension UITableViewCell {
         // must add this, otherwise default is 0, now actual indentation = indentationWidth * indentationLevel = 10000 * 1 = -10000
         self.indentationLevel = 1
     }
-
-/*
-    func setTableViewCellSeparatorLeftInset0(tableViewCell:UITableViewCell) {
-        //make separator left align to edge
-        tableViewCell.separatorInset = UIEdgeInsetsZero
-        tableViewCell.layoutMargins = UIEdgeInsetsZero
-        //tableViewCell.layoutMargins.left = 0
-        tableViewCell.preservesSuperviewLayoutMargins = false
-    }
- 
-    func setTableViewCellSeparatorBothInset(tableViewCell:UITableViewCell){
-        let AllPageLeftRightPadding:CGFloat = 15.0
-        
-        tableViewCell.separatorInset = UIEdgeInsetsMake(0, AllPageLeftRightPadding, 0, AllPageLeftRightPadding)
-        tableViewCell.layoutMargins = UIEdgeInsetsZero
-        // tableViewCell.preservesSuperviewLayoutMargins = false
-    }
-*/
     
     //make separator left align to edge
     func setSeparatorLeftAlign() {
-        self.setSeparatorLeftRightAlign(0)
-
+        self.setSeparatorLeftRightAlign(-100, rightAlign: -100)
+    }
+    
+    func setSeparatorLeftRightAlign(_ leftAlign:CGFloat = 0, rightAlign:CGFloat = 0){
+        self.separatorInset = UIEdgeInsetsMake(0, leftAlign, 0, rightAlign)
+        
+        //self.tableView.separatorStyle = UITableViewCellSeparatorStyle.SingleLine
+        //self.separatorInset.left.add(leftAlign) //
+//        self.separatorInset.left = 0//leftAlign
+//        self.separatorInset.right = rightAlign
+        self.layoutMargins = UIEdgeInsets.zero
         self.preservesSuperviewLayoutMargins = false
     }
     
-    func setSeparatorLeftRightAlign(leftAlign:CGFloat = 0, rightAlign:CGFloat = 0){
-        self.separatorInset = UIEdgeInsetsMake(0, leftAlign, 0, rightAlign)
-        self.layoutMargins = UIEdgeInsetsZero
-        // tableViewCell.preservesSuperviewLayoutMargins = false
-    }
-
 }
