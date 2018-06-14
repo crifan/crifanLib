457f50ddaa90c578e8b873394944285acffdf064
naturling_crifan <crifan.li@naturling.com>
2018-06-14 15:32:52 +0800

    1. update python crifanDict

--- python/crifanLib/crifanDict.py
+++ python/crifanLib/crifanDict.py
@@ -46,27 +46,111 @@ gConst = {
 ################################################################################
 
 
-def recursiveMergeDict(originalDict, toMergeDict):
+def recursiveMergeDict(aDict, bDict):
     """
-    Recursively merge origin and to merge dict, return merged dict.
-    Sub dict's won't be overwritten but also updated.
+    Recursively merge dict a to b, return merged dict b
+    Note: Sub dict and sub list's won't be overwritten but also updated/merged
+
+    example:
+(1) input and output example:
+input:
+{
+  "keyStr": "strValueA",
+  "keyInt": 1,
+  "keyBool": true,
+  "keyList": [
+    {
+      "index0Item1": "index0Item1",
+      "index0Item2": "index0Item2"
+    },
+    {
+      "index1Item1": "index1Item1"
+    },
+    {
+      "index2Item1": "index2Item1"
+    }
+  ]
+}
+
+and
+
+{
+  "keyStr": "strValueB",
+  "keyInt": 2,
+  "keyList": [
+    {
+      "index0Item1": "index0Item1_b"
+    },
+    {
+      "index1Item1": "index1Item1_b"
+    }
+  ]
+}
+
+output:
+
+{
+  "keyStr": "strValueB",
+  "keyBool": true,
+  "keyInt": 2,
+  "keyList": [
+    {
+      "index0Item1": "index0Item1_b",
+      "index0Item2": "index0Item2"
+    },
+    {
+      "index1Item1": "index1Item1_b"
+    },
+    {
+      "index2Item1": "index2Item1"
+    }
+  ]
+}
+
+(2) code usage example:
+import copy
+cDict = recursiveMergeDict(aDict, copy.deepcopy(bDict))
+
+Note:
+bDict should use deepcopy, otherwise will be altered after call this function !!!
+
     """
-    originalDictItems = None
+    aDictItems = None
+    if crifanSystem.isPython2(): # is python 2
+      aDictItems = aDict.iteritems()
+    else: # is python 3
+      aDictItems = aDict.items()
 
-    if crifanSystem.isPython2():
-        originalDictItems = originalDict.iteritems()
-    else:  # is python 3
-        originalDictItems = originalDict.items()
+    for aKey, aValue in aDictItems:
+      # print("------ [%s]=%s" % (aKey, aValue))
+      if aKey not in bDict:
+        bDict[aKey] = aValue
+      else:
+        bValue = bDict[aKey]
+        # print("aValue=%s" % aValue)
+        # print("bValue=%s" % bValue)
+        if isinstance(aValue, dict):
+          recursiveMergeDict(aValue, bValue)
+        elif isinstance(aValue, list):
+          aValueListLen = len(aValue)
+          bValueListLen = len(bValue)
+          bValueListMaxIdx = bValueListLen - 1
+          for aListIdx in range(aValueListLen):
+            # print("---[%d]" % aListIdx)
+            aListItem = aValue[aListIdx]
+            # print("aListItem=%s" % aListItem)
+            if aListIdx <= bValueListMaxIdx:
+              bListItem = bValue[aListIdx]
+              # print("bListItem=%s" % bListItem)
+              recursiveMergeDict(aListItem, bListItem)
+            else:
+              # print("bDict=%s" % bDict)
+              # print("aKey=%s" % aKey)
+              # print("aListItem=%s" % aListItem)
+              bDict[aKey].append(aListItem)
 
-    mergedDict = toMergeDict.copy()
+    return bDict
 
-    for key, value in originalDictItems:
-        if key not in mergedDict:
-            mergedDict[key] = value
-        elif isinstance(value, dict):
-            recursiveMergeDict(value, mergedDict[key])
-
-    return mergedDict
 
 ################################################################################
 # Test
