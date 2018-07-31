8dbee5bb68146f232bffd2cde97d7fa9b4d0a49d
naturling_crifan <crifan.li@naturling.com>
2018-07-31 10:37:38 +0800

    1. added js storage getObj and setObj

---
+++ javascript/storage.js
@@ -0,0 +1,34 @@
+/*
+ * Javascript storage realted functions
+ * 
+ * Crifan Li
+ * Updated: 20180731
+ * 
+ */
+
+/*
+  Save js object to local storage via json string
+  call example: localStorage.setObj("currentUser", currentUser)
+*/
+Storage.prototype.setObj = function(strKey, objValue) {
+  // console.log("Storage.prototype.setOb: strKey=", strKey, ", objValue=", objValue)
+  const jsonStr = JSON.stringify(objValue)
+  // console.log("jsonStr=", jsonStr)
+  return this.setItem(strKey, jsonStr)
+}
+
+/*
+  Restore js object from local storage's stored json string
+  call example: restoredCurrentUser = localStorage.getObj("currentUser")
+*/
+Storage.prototype.getObj = function(strKey) {
+  // console.log("Storage.prototype.getObj: strKey=", strKey)
+  let parsedObj
+  const storedStr = this.getItem(strKey)
+  // console.log("storedStr=", storedStr)
+  if (storedStr) {
+    parsedObj = JSON.parse(storedStr)
+  }
+  // console.log("parsedObj=", parsedObj)
+  return parsedObj
+}
